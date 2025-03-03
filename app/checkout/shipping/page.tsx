fce7d3c87ded518c51c4b24a498db084473784da
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-03-03 18:11:34 +0100

    Add initial checkout progress bar

--- app/checkout/shipping/page.tsx
+++ app/checkout/shipping/page.tsx
@@ -1,9 +1,11 @@
 import { getCountryNamesForQuery } from "@/app/checkout/actions";
 import AddressFormsContainer from "@/app/ui/shipping/AddressFormsContainer";
+import ProgressBar from "@/app/ui/ProgressBar";
 
 export default function ShippingPage() {
   return (
     <div>
+      <ProgressBar activeLabel="Shipping & Billing" />
       <h1>Shipping</h1>
       <AddressFormsContainer getCountries={getCountryNamesForQuery} />
     </div>
---
+++ app/ui/ProgressBar.tsx
@@ -0,0 +1,31 @@
+interface Props {
+  activeLabel: string;
+}
+
+export default function ProgressBar({ activeLabel }: Props) {
+  const steps = [
+    {
+      label: "Shipping & Billing",
+      active: activeLabel === "Shipping & Billing",
+    },
+    {
+      label: "Review Order",
+      active: activeLabel === "Review Order",
+    },
+    {
+      label: "Payment",
+      active: activeLabel === "Payment",
+    },
+    {
+      label: "Complete",
+      active: activeLabel === "Complete",
+    },
+  ];
+  return (
+    <ol className="flex">
+      {steps.map((step) => (
+        <li key={step.label}>{step.label}</li>
+      ))}
+    </ol>
+  );
+}
