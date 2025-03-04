3fc93ba6a7ce4a8ca5ad61fc7e3aa90551bb4eed
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-03-04 10:48:24 +0100

    feat: improve progress breadcrumbs
    - Display dummy 'V' when step complete
    - Change last step from 'Complete' to 'Order complete'
    - Improve styling to display progress bar with step number and name

--- app/ui/ProgressBreadcrumbs.tsx
+++ app/ui/ProgressBreadcrumbs.tsx
@@ -20,17 +20,28 @@ export default function ProgressBreadcrumbs({ activeLabel }: Props) {
       active: activeLabel === "Payment",
     },
     {
-      label: "Complete",
-      href: "complete",
-      active: activeLabel === "Complete",
+      label: "Order complete",
+      href: "order-complete",
+      active: activeLabel === "Order complete",
     },
   ];
+
+  const activeLabelIndex = steps.findIndex(
+    (step) => step.label === activeLabel,
+  );
+
   return (
-    <nav className="h-16 border border-b-black">
-      <ol className="flex justify-center gap-12">
-        {steps.map((step) => (
-          <li key={step.label}>
-            <a href={step.href}>{step.label}</a>
+    <nav className="border-b-black w-full">
+      <ol className="flex justify-center gap-4 py-12">
+        {steps.map((step, index) => (
+          <li key={step.label} className="flex flex-col gap-2">
+            <div className="w-60 h-1 bg-black"></div>
+            <div className="flex gap-4 items-center">
+              <div className="w-10 h-10 bg-black rounded-3xl text-white flex items-center justify-center">
+                {activeLabelIndex > index ? "V" : `${index + 1}`}
+              </div>
+              <div>{step.label}</div>
+            </div>
           </li>
         ))}
       </ol>
