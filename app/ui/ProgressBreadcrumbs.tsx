41ae954776e602e01372663002626ccc3528a576
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-03-04 12:16:15 +0100

    style: add svg and colors to progress breadcrumbs
    - Add a check svg to the finished step
    - Add green/gray color to steps depending on the progress

--- app/ui/ProgressBreadcrumbs.tsx
+++ app/ui/ProgressBreadcrumbs.tsx
@@ -1,3 +1,5 @@
+import { clsx } from "clsx";
+
 interface Props {
   activeLabel: string;
 }
@@ -26,19 +28,58 @@ export default function ProgressBreadcrumbs({ activeLabel }: Props) {
     },
   ];
 
-  const activeLabelIndex = steps.findIndex(
+  const activeLabelIndex: number = steps.findIndex(
     (step) => step.label === activeLabel,
   );
+
+  const checkSvg = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="white"
+      height="24"
+      width="24"
+    >
+      <path
+        fillRule="evenodd"
+        d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+
+  function progressCircleClass(index: number): string {
+    if (index < activeLabelIndex) {
+      return "bg-green-500";
+    } else if (index === activeLabelIndex) {
+      return "border-2 border-green-500 text-green-500 text-lg";
+    } else {
+      return "border-2 border-gray-400 text-gray-400 font-medium";
+    }
+  }
+
+  function progressBarStyle(index: number): string {
+    if (index < activeLabelIndex || index === activeLabelIndex) {
+      return "bg-green-500";
+    } else {
+      return "bg-gray-400";
+    }
+  }
 
   return (
     <nav className="border-b-black w-full">
       <ol className="flex justify-center gap-4 py-12">
         {steps.map((step, index) => (
           <li key={step.label} className="flex flex-col gap-2">
-            <div className="w-60 h-1 bg-black"></div>
+            <div className={clsx("w-60 h-1", progressBarStyle(index))}></div>
             <div className="flex gap-4 items-center">
-              <div className="w-10 h-10 bg-black rounded-3xl text-white flex items-center justify-center">
-                {activeLabelIndex > index ? "V" : `${index + 1}`}
+              <div
+                className={clsx(
+                  "w-10 h-10 rounded-3xl flex items-center justify-center",
+                  progressCircleClass(index),
+                )}
+              >
+                {activeLabelIndex > index ? checkSvg : `${index + 1}`}
               </div>
               <div>{step.label}</div>
             </div>
