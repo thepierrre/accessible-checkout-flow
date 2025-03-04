9d10a67347d6f6db7292acfe57822323a12e719f
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-03-04 09:10:38 +0100

    feat: add zod address schema

--- app/checkout/models.ts
+++ app/checkout/models.ts
@@ -1,5 +1,17 @@
+import { z, ZodType } from "zod";
+
 export type CountriesInfo = {
   [name: string]: number[];
 };
 
 export type AddressType = "shipping" | "billing";
+
+export const AddressSchema: ZodType<FormData> = z.object({
+  name: z.string(),
+  address: z.string(),
+  zip: z.string(),
+  region: z.string().optional(),
+  country: z.string(),
+  phone: z.string(),
+  email: z.string().email(),
+});
