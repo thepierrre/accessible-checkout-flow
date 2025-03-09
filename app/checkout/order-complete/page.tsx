12e156ce5e569a00a90a02624fb2afd1189b7982
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-03-09 17:40:25 +0100

    refactor: change breadcrumbs component name to 'Stepper' (2)

--- app/checkout/order-complete/page.tsx
+++ app/checkout/order-complete/page.tsx
@@ -3,7 +3,7 @@ import Stepper from "@/app/ui/Stepper";
 export default function OrderCompletePage() {
   return (
     <div className="flex flex-col items-center">
-      <ProgressBreadcrumbs activeLabel="Order complete" />
+      <Stepper activeLabel="Order complete" />
       <main className="flex flex-col gap-8 w-112"></main>
     </div>
   );
