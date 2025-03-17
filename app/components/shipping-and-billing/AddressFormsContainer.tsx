c1e8bee4040a666ebe841750729a77a16d5070ba
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-03-17 16:58:37 +0100

    fix: fix displaying billing address on edit
    - Fix the useEffect hook for form so that the page is scrolled to the billing form on edit only on the first render
    -

--- app/components/shipping-and-billing/AddressFormsContainer.tsx
+++ app/components/shipping-and-billing/AddressFormsContainer.tsx
@@ -123,7 +123,7 @@ export default function AddressFormsContainer({
         behavior: "instant",
       });
     }
-  });
+  }, [isEditing]);
 
   useEffect(() => {
     if (billingCheckboxRef.current) {
