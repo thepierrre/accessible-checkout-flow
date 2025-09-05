fa9839408fc2921cd1697abc464668dcd166f741
thepierrre <piotr.owczarczyk98@gmail.com>
2025-09-05 19:48:49 +0200

    fix style detail in ProcessingPayment.tsx

--- app/components/order-complete/ProcessingPayment.tsx
+++ app/components/order-complete/ProcessingPayment.tsx
@@ -5,7 +5,7 @@ export default function ProcessingPayment() {
         Connecting to your card provider
       </h1>
       <p className="text-gray-600 text-lg">
-        Processing your payment <span className="animate-pulse">...</span>
+        Processing your payment<span className="animate-pulse">...</span>
       </p>
       <div className="flex space-x-2">
         <div className="h-3 w-3 rounded-full bg-blue-primary animate-bounce [animation-delay:-0.3s]" />
